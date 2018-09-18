34c2e6ce0125ae8bdc28a6f677b071fcb424f948
HarryLottey <harry.lottey@gmail.com>
2018-09-18 20:52:08 +1000

    First Person Template

---
+++ Assets/Scripts/MeleeAnimationHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//////////////////////////////////////////////////////////////////////////
+//
+// This script handles the animations of the melee attacks of the player.
+//
+/////////////////////////////////////////////////////////////////////////
+
+public class MeleeAnimationHandler : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
