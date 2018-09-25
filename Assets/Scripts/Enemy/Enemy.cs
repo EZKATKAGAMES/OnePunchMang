75ef51d7f44e51bfe3ad0169b2945057e325c517
Ombii <39289530+Ombii@users.noreply.github.com>
2018-09-25 11:36:22 +1000

    Small edits, AI begin

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public float distanceToEngage = 10f;
+    public float distanceToCircle = 5f;
+    public NavMeshAgent agent;
+    public Vector3 target;
+    public string targetID;
+
+    public enum States
+    {
+        Circling, Attack, Idle
+    }
+
+    public States currentState;
+
+    private void Awake()
+    {
+        targetID = Resources.Load("Prefabs/Characters/PlayerCharacter").name as string;
+    }
+
+    void Start()
+    {
+        Debug.Log(targetID);
+
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        // Default state.
+        currentState = States.Idle;
+    }
+
+    
+    void Update()
+    {
+        // Indetify target
+        target = GameObject.Find(targetID).GetComponent<Transform>().position;
+        
+
+        // Initialize circling state.
+        if(Vector3.Distance(gameObject.transform.position, target) <= distanceToEngage)
+        {
+            currentState = States.Circling;
+        }
+
+        // Initialize attack state.
+        if(currentState == States.Circling)
+        {
+
+        }
+
+        // Circle state functions
+        if(currentState == States.Circling)
+        {
+            // Look at the target
+            Vector3 relativePosition = target - gameObject.transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePosition);
+            transform.rotation = rotation;
+
+            // Circle around the target
+
+
+        }
+
+
+
+
+    }
+
+
+
+}
